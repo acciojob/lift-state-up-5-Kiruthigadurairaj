8331fc2bbd99a9419d6029933d656a1ace7163f9
kiruthiga durairaj <kiruthigalatha05@gmail.com>
2025-03-16 16:42:21 +0530

    my first message

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import './../styles/App.css';
-import LoginForm from "./LoginForm";
+import Parent from "./Parent";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
     <div>
-      <h1>Parent Component</h1>
-      {isLoggedIn ? (
-        <h2>Welcome, User!</h2>
-      ) : (
-        <LoginForm onLogin={handleLogin} />
-      )}
+      {/* Do not remove the main div */}
+      <Parent />
     </div>
   );
 };
 
-export default App;
+export default App; 
